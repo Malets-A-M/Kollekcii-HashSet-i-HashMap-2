a1f7ee352cd8afd9314b858d8a65642e84a8d32f
Salam4ik <andrej160195@mail.ru>
2023-01-28 00:00:45 +0300

    Вынес Set<String> set = new HashSet<>() и String[] words = text.split("\\P{IsAlphabetic}+") из методов

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,17 +1,22 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class WordsChecker {
     private String text;
+    private Set<String> set = new HashSet<>();
+    private String[] words = text.split("\\P{IsAlphabetic}+");
+
 
     public WordsChecker(String text) {
         this.text = text;
     }
     public boolean hasWord(String word){
-        String[] words = text.split("\\P{IsAlphabetic}+");
-    return Arrays.asList(words).contains(word);
+        set.addAll(List.of(words));
+        return set.contains(word);
     }
     public void printText(){
-        String[] words = text.split("\\P{IsAlphabetic}+");
         for (String word : words){
             System.out.println(word);
         }
